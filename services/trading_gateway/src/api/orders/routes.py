836b5aa9c9d1e294ab56196522f1c7c7175d6333
amoghkini <amoghkini@gmail.com>
2025-03-22 22:06:54 +0000

    add new order to queu basic implementation

--- services/trading_gateway/src/api/orders/routes.py
+++ services/trading_gateway/src/api/orders/routes.py
@@ -12,8 +12,23 @@ async def create_order(
     order_data: OrderCreateModel
 ) -> JSONResponse:
     print(order_data)
+    add_to_queue(order_data)
     return JSONResponse(
         content={
             "message": "Order placed successully"
         }
     )
+    
+def add_to_queue(order_data):
+    import redis
+    import os
+
+    queue = redis.Redis(
+        host=os.getenv("QUEUE_REDIS_HOST", "exchange_redis"),
+        port=int(os.getenv("QUEUE_REDIS_PORT", 6379)),
+        db=0
+    )
+    order_data_json = order_data.json()
+    print("Adding order to queue:", order_data_json)
+    queue.lpush("orders_queue", order_data_json)
+    print("Order added to queue")
