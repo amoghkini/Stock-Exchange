c6a8a34f2a653e4d6f5da990c29ad0677e13e0eb
Amogh Kini <amoghkini@gmail.com>
2025-06-20 02:47:51 +0530

    added generic exceptions

---
+++ services/instrument_registry/instrument_registry/app/exceptions.py
@@ -0,0 +1,14 @@
+class NoContextProcessorException(Exception):
+    pass
+
+
+class NoExtensionException(Exception):
+    pass
+
+
+class NoInstalledBlueprintException(Exception):
+    pass
+
+
+class NoRouteModuleException(Exception):
+    pass
